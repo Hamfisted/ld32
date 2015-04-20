3d4c444168c3c4ef2b1472ec2522d99cc9afa6e3
Jordan Cristiano <jordan.cristi@gmail.com>
2015-04-19 21:04:29 -0400

    Added some jiggling to the seed pickups

--- source/SeedPickup.hx
+++ source/SeedPickup.hx
@@ -8,14 +8,26 @@ import flixel.util.FlxColor;
 
 class SeedPickup extends FlxSprite
 {
+  var _nextJumpMilli:Int;
+
   public function new(X:Float=0, Y:Float=0)
   {
     super(X+4, Y);
     makeGraphic(8, 8, FlxColor.LIME);
+    this.acceleration.y = Reg.GRAVITY;
+
+    _nextJumpMilli = FlxG.game.ticks + Std.random(1000);
   }
 
   override public function update():Void
   {
+    if (_nextJumpMilli < FlxG.game.ticks && isTouching(FlxObject.FLOOR))
+    {
+      _nextJumpMilli = FlxG.game.ticks + Std.random(1000) + 500;
+      var jumpTime:Float = (Std.random(2) == 0) ? 0.15 : 0.25;
+      BounceMovement.BounceTo(this, this.getMidpoint(), this.acceleration.y, jumpTime);
+      this.velocity.x = 0.0;
+    }
     super.update();
   }
 
